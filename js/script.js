e4e24591de6d7b8e375f4bafa06b2d384b308f9e
reham90 <rehamadel344@gmail.com>
2024-07-03 17:31:10 +0300

    edit

--- js/script.js
+++ js/script.js
@@ -129,6 +129,37 @@ document.getElementById('saturday').onchange = function() {
  
 };
 
+
+/////////////////////////// toggle check faq //////////////////////
+      // http://stackoverflow.com/a/9762070/938089?why-is-the-selector-not-working
+
+      $(".accordion__title").click(function(event) {
+        var radio_selector = 'input[type="radio"]',
+            $radio;
+    
+        // Ignore the event when the radio input is clicked.
+        if (!$(event.target).is(radio_selector)) {
+            $radio = $(this).find(radio_selector);
+            // Prevent the event to be triggered
+            // on another element, for the same click
+            event.stopImmediatePropagation();
+            // We manually check the box, so prevent default
+            event.preventDefault();
+            $radio.prop('checked', !$radio.is(':checked'));
+        }
+    });
+    $(".accordion__title input").on('change click', function(event) {
+        // The change event only fires when the checkbox state changes
+        // The click event always fires
+        
+        // When the radio is already checked, this event will fire only once,
+        // resulting in an unchecked checkbox.
+        // When the radio is not checked already, this event fires twice
+        // so that the state does not change
+        this.checked = !this.checked;
+    })
+
+
 //////////////////////// delete record ///////////////////////////////////
 $(document).on('click', '.delete-btn', function() {
     $(this).parent().remove();
